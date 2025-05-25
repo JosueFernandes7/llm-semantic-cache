f98188567cce7dbac56347ac0c8992cb5e45479a
Josué Fernandes <josue7mf@hotmail.com>
2025-05-24 23:29:07 -0300

    feat: add docker to environment

--- core/settings.py
+++ core/settings.py
@@ -8,7 +8,8 @@ load_dotenv()
 OLLAMA_HOST = os.getenv("OLLAMA_HOST") or "http://localhost:11434"
 
 # Redis configuration
-REDIS_URL = os.getenv("REDIS_URL") or "redis://:mypassword@localhost:6379"
+REDIS_PASSWORD = os.getenv("REDIS_PASSWORD") or "mypassword"
+REDIS_URL = os.getenv("REDIS_URL") or f"redis://:{REDIS_PASSWORD}@localhost:6379"
 INDEX_NAME = os.getenv("INDEX_NAME") or "faq_index"
 
 redis_config = RedisConfig(index_name=INDEX_NAME, redis_url=REDIS_URL)
